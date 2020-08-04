75c907feaeb8b50b4a8f60d497c1cd768b533fc7
X Newbie <69149585+X-Newbie@users.noreply.github.com>
2020-08-04 20:59:57 +0700

    Update stickers.py

--- userbot/modules/stickers.py
+++ userbot/modules/stickers.py
@@ -103,7 +103,7 @@ async def kang(args):
                 emoji = splat[1]
 
         packname = f"{user.username}{pack}"
-        packnick = f"{user.fullname} ~{pack}"
+        packnick = f"{user.lastname} ~{pack}"
         cmd = "/newpack"
         file = io.BytesIO()
 
@@ -135,7 +135,7 @@ async def kang(args):
                 while "120" in x.text:
                     pack += 1
                     packname = f"{user.username}{pack}"
-                    packnick = f"{user.fullname} Animated ~{pack}"
+                    packnick = f"{user.lastname} Animated ~{pack}"
                     await args.edit(
                         "`Switching to Pack "
                         + str(pack)
